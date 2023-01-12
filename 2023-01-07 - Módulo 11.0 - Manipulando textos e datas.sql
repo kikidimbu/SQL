17a75db037f0183efa6b4cde0e1a5d302e8dbfcb
Jaqueline (Kiki) Dimbu <kikidimbu@gmail.com>
2023-01-12 19:45:36 -0300

    Update 2023-01-07 - Módulo 11.0 - Manipulando textos e datas.sql

--- 2023-01-07 - Módulo 11.0 - Manipulando textos e datas.sql
+++ 2023-01-07 - Módulo 11.0 - Manipulando textos e datas.sql
@@ -23,9 +23,24 @@ LEN e DATA DATALENGTH: Retorna a quantidade de caraccteres de uma palavra.*/
 SELECT
 	LEN('SQL Hash   tag') AS 'Len', -- Considera todos os espaços entre palavras
 	DATALENGTH(' SQL Has   htag   ') AS 'DataLength' -- Considera todos os espaços antes e depois das palavras
-/* -------------------------------------------------------------------------------------------------*/
+/* -------------------------------------------------------------------------------------------------
+CONCAT: Permite juntar mais de um texto em uma única palavra.*/
+-- Faça uma consulta à tabela DimCustomer onde seja possível visualizar o nome completo de cada cliente.
 
-/* -------------------------------------------------------------------------------------------------*/
+SELECT
+	FirstName AS 'Nome',
+	LastName AS 'Sobrenome',
+	CONCAT (FirstName, ' ',  LastName) AS 'Nome Completo',
+	EmailAddress AS 'E-mail'
+FROM 
+	DimCustomer
+
+/* -------------------------------------------------------------------------------------------------
+LEFT AND RIGHT
+LEFT: Extrai uma determinada quantidade de caracteres de um texto, da esquerda para a direita.
+RIGHT: Extrai uma determinada quantidade de caracterere de um texto, da direita para a esquerda.*/
+--
+
 
 /* -------------------------------------------------------------------------------------------------*/
 
